1fb8ca7511c4f55c1af4b600137dd97c23d8525c
Alejandro Dubon <alejandrodubon88@gmail.com>
2020-05-19 01:39:37 -0600

    juego de adivina la imagen funcioanlidades terminadas

--- js/adivinaLaImagen.js
+++ js/adivinaLaImagen.js
@@ -13,7 +13,7 @@
 
     //-----------------------------------------------OtherFunctions
 
-   
+
 
     function animeImageTemplate(anime){
         return `
@@ -50,45 +50,64 @@
             let number_c = generateRandomNumber(30);   
             if ((number_a != number_b) && (number_a != number_c) && (number_b != number_c)) {
                 bandera = 1
-                console.log('todos son diferentes');
                 const newArray = [number_a, number_b, number_c];
                 return newArray
             } 
         }
     }
 
+    function generateName(index){
+        const animes = ['Naruto: Shippuuden Movie 3 - Hi no Ishi wo Tsugu Mono', 'Hunter x Hunter', 'Attack on titan', 'Dungeon ni Deai wo Motomeru no wa Machigatteiru Darou ka', 'Oregairu', 'Evangelion', 'Dragon Ball: Episode of Bardock', '	Fullmetal Alchemis', '	Yu☆Gi☆Oh! (Movie)', 'Boku no pico', 'Haikyu!!', 'Kaguya sama', 'Tate no Yuusha no Nariagari', 'Darling in the Franxx', 'Clannad', 'Toradora!', 'Kimi ni todoke', 'Rosario to Vampire', 'Akame ga Kill!', 'Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai', 'Youkoso Jitsuryoku Shijou Shugi no Kyoushitsu e (TV)', 'Yakusoku no Neverland', 'Kono Subarashii Sekai ni Shukufuku wo!', 'Kishuku Gakkou no Juliet', 'Another', 'Shokugeki no Souma', 'Gintama', 'Tengen Toppa Gurren Lagann', 'Death Note', 'Plastic Memories', 'One punch man', 'Fairy tail 2', 'Domestic no kanojo', 'Hyouka', 'Shigatsu Wa kimi no uso', 'Oresuki', 'Bleach', 'Mirai nikki', 'Elfed lied', '	Yamada-kun to 7-nin no Majo (TV)', 'Golden Time', 'dororo', 'Fire force', 'Blue exorcist', 'Dr. Stone', 'Angel beats!', 'Charlotte','Highschool DXD','Kami nomi zo shiru sekai']
+        return animes[index];
+    }
+
+    function reloadGame() {
+        location.reload();
+    }
+    function addEventClickError($element) {
+        $element.addEventListener('click', () => {
+            swal("Tu respuesta es incorrecta!", "Se reiniciara el juego en breve", "error");
+            
+            setTimeout(reloadGame, 5000)
+        })        
+    }
+    function addEventClickSuccess($element, index) {
+        $element.addEventListener('click', () => {
+            index++
+            if (index === 50) {
+                swal("Felicidades!!!!!!!", "Juego terminado, se nota que no te bañas", "Success");
+            }else{
+                inicialiar(index)
+            }
+        })        
+    }
+
     function fillEachOption($element, index){
-        const randomAnimes = ['Haikyuu', 'Danmachi', 'Shokugeki No soma', 'Apare Ranma', 'Kimi ni todoke', 'Boku no pico', 'Toradora!', 'Clannad', 'The rising of the shield hero', 'Rosario+Vampire', 'Highschool DXD', 'Fornite', 'Karmaland', 'Neon genesis evangelion', 'Shigatsu Wa kimi no uso', 'Dororo', 'Full metal Panic!', 'Inuyasha', 'Darling in the Franxx', 'Dr. Stone', 'Yamada Kun to 7-nin no majo', 'Oregairu', 'Oreimo', 'Fairy tail', 'Bleach', 'Shokugeki no soma', 'Fire Force', 'Blue Exorcist', 'Angel Beats!', 'Oresuki', 'Hyouka'];
+        const randomAnimes = ['Digimon adventure (2020)', 'Gundaman build divers', 'Orange', 'Apare Ranma', 'House of cards', 'Ahiru no sora', 'Apocalipsis minecraft', 'Drake & Josh', 'The rising of the shield hero', 'Highschool Musical', 'Kill la kill', 'Angels of death', 'Karmaland', 'Blend-s', 'Keep your hands off eizouken!', 'Big order', 'Nanbaka', 'Inuyasha', 'Nisekoi', 'Gate', 'Yamada Kun to 7-nin no majo', 'Tsugumomo', 'Hundred', 'Fairy tail', 'Himouto! Umaru-chan', 'Berserk', 'A certain magical index', 'Akatsuki no yona', 'Gamers', 'Isekai cheat magician', 'Free!'];
         $element.children[0].remove();
         const HTMLString_optionTitle = optionAnimeGenericTemplate(randomAnimes[index]);
         const optionTitle = createTemplate(HTMLString_optionTitle);
         $element.append(optionTitle);
+
+        addEventClickError(optionTitle);
     }
 
-    function fillAllOptions(options){
+        
+
+
+    function fillAllOptions(options, anime, index){
         const optionsLength = options.length;
         const random = generateRandomNumber(optionsLength);
-        console.log(random);
-        if (random == 4) {
-            alert('random es igual a 4')            
-        }
+        
         //DOM random element (option)
         const $randomOption = options[random]
         $randomOption.children[0].remove();
         const HTMLString_animeTitle = optionAnimeTemplate(anime);
         const animeTitle = createTemplate(HTMLString_animeTitle);
         $randomOption.append(animeTitle);
-
-        
+        addEventClickSuccess(animeTitle, index);
 
         let uniqueNumbers = generate3diferentRandomNumbers();
-        console.log('este es unique numbers'+ uniqueNumbers);
-        if(uniqueNumbers){
-            console.log('true');
-        }else{
-            console.log('false');
-        }
-        
         //llenar las opciones restantes
         switch (random) {
             case 0:
@@ -120,35 +139,33 @@
                 break;
         }
 
-
     }
 
-    function renderAnime(anime, $element, options){
+
+    function renderAnime(anime, $element, options, index){
         $element.children[0].remove();
         const HTMLString_animeImage = animeImageTemplate(anime);
         const animeImage = createTemplate(HTMLString_animeImage);
         $element.append(animeImage);
 
-        fillAllOptions(options);
-        
-
+        fillAllOptions(options, anime, index);
 
     }
-
-    const animeName = 'Domestic no kanojo'
-    const {results: { 0: anime}} = await getAnimes(`${BASE_URL}?q=${animeName}&page=1&limit=1`);
-    console.log(anime);    
+    
     const $gameImageContainer = document.getElementById('game-image-container');
-    const $loader = document.getElementById('loader')
-
     const $option1 = document.getElementById('option-1');
     const $option2 = document.getElementById('option-2');
     const $option3 = document.getElementById('option-3');
     const $option4 = document.getElementById('option-4');
     const options_array = [$option1, $option2, $option3, $option4]
+        
     
-
+    async function inicialiar(index){
+        const animeName = generateName(index)
+        const {results: { 0: anime}} = await getAnimes(`${BASE_URL}?q=${animeName}&page=1&limit=1`);
+        renderAnime(anime, $gameImageContainer, options_array, index);
+    }
     
-    renderAnime(anime, $gameImageContainer, options_array);
+    inicialiar(0)    
     
 })()
